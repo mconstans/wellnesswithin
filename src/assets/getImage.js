3c1e4e7013d906cfc100a5fea52584272a6152e2
mconstans <megan.constans@gmail.com>
2024-06-19 11:49:01 -0500

    added image assets

---
+++ src/assets/getImage.js
@@ -0,0 +1,12 @@
+import imgOne from "./portfolioImages/1.jpg";
+import imgTwo from "./portfolioImages/2.jpg";
+import imgThree from "./portfolioImages/3.jpg";
+import imgFour from "./portfolioImages/4.jpg";
+import imgFive from "./portfolioImages/5.jpg";
+import imgSix from "./portfolioImages/6.jpg";
+import imgSeven from "./portfolioImages/7.jpg";
+import imgEight from "./portfolioImages/8.jpg";
+import imgNine from "./portfolioImages/9.jpg";
+import contact1 from "./contact1.png";
+
+export { imgOne, imgTwo, imgThree, imgFour, imgFive, imgSix, imgSeven, imgEight, imgNine, contact1 };
